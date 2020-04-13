8f1c14809cf9700db980e4c2eae7a9f384f487f3
Tolgonay <Tolgonay@MacBook-Air-Aida.local>
2020-04-13 17:20:53 +0200

    Initial commit

---
+++ src/com/company/Boss.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Boss extends GameEntity {
+    private String Weapon;
+
+    public Boss (String Weapon); { this.Weapon = Weapon; }
+
+    public String getWeapon() { return Weapon; }
+
+    public void setWeapon(String weapon) { Weapon = weapon; }
+
+    public String info(Weapon) { }
+}
---
+++ src/com/company/Weapon.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Weapon {
+    private String type;
+    private String name;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
+}
